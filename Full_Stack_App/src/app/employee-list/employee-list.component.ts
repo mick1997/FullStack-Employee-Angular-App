8080d0bef86846a8539c46cc4de897e21d03b23b
mick1997 <mickjack2013@gmail.com>
2023-06-26 13:28:03 -0400

    add update feature

--- Full_Stack_App/src/app/employee-list/employee-list.component.ts
+++ Full_Stack_App/src/app/employee-list/employee-list.component.ts
@@ -7,14 +7,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent {
-
   employees!: Observable<Employee[]>;
 
-  constructor(private employeeService: EmployeeService,
-              private router: Router) {}
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.reloadData();
@@ -23,17 +24,23 @@ export class EmployeeListComponent {
   reloadData() {
     this.employees = this.employeeService.getEmployeeList();
   }
-  
+
   deleteEmployee(id: number) {
-    this.employeeService.deleteEmployee(id)
-      .subscribe(data => {
+    this.employeeService.deleteEmployee(id).subscribe(
+      (data) => {
         // print delete employee info into console
         console.log(data);
         this.reloadData();
       },
-      error => console.log(error));
+      (error) => console.log(error)
+    );
   }
 
   employeeDetails(id: number) {
-    this.router.navigate(['details', id])};
+    this.router.navigate(['details', id]);
+  }
+
+  updateEmployee(id: number) {
+    this.router.navigate(['update', id]);
+  }
 }
